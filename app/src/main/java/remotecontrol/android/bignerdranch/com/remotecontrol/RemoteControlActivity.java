6b10657447790fdc395b607edcca0a4c7f61e4cc
Tyler <birdty@gmail.com>
2016-05-28 19:56:25 -0600

    Initial commit

---
+++ app/src/main/java/remotecontrol/android/bignerdranch/com/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,22 @@
+package remotecontrol.android.bignerdranch.com.remotecontrol;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class RemoteControlActivity extends SingleFragmentActivity
+{
+    @Override
+    protected Fragment createFragment()
+    {
+        return new RemoteControlFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+    }
+}
